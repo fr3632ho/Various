4614e6f7fe414f41c60086337938532512874790
fr3632ho <fredrik@dannert.se>
2020-03-26 20:05:31 +0100

    Update in comments to brent

--- simple/brent.py
+++ simple/brent.py
@@ -1,4 +1,13 @@
 import math
+"""
+Implementation of Brent's 'Tortoise & Hare' based of of Floyd's but a bit faster.
+The algorithm is a pointer based algorithm which detects cycles of any given length,
+where the cycle begins from the starting point in the sequence and a repeated value.
+The algorithm compares two pointers moving at different speeds; the tortoise and the hare.
+If the hare catches up to the tortoise, thus sharing the same index again in the sequence,
+a cycle exists in the sequence. Brent's algorithm searches for the smalles power of
+2**i which is larger than borth lambda and mu for i = 0,1,2...
+"""
 
 sequence = [1,2,3,4,5,6,6,7,8,9,10]
 
@@ -36,7 +45,9 @@ def brent(x0):
     return lam,mu,hare
 
 x0 = 0
+x1 = 4
 lam,mu,value = brent(sequence[x0])
+lam1,mu1,value1 = brent(sequence[x1])
 print(
 '''
 start-index: {},
@@ -44,3 +55,11 @@ sequence: {},
 cycle-length: {},
 index from starting point (positive direction): {},
 repeated value: {}'''.format(x0,sequence,lam,mu,value))
+
+print(
+'''
+start-index: {},
+sequence: {},
+cycle-length: {},
+index from starting point (positive direction): {},
+repeated value: {}'''.format(x1,sequence,lam1,mu1,value1))
