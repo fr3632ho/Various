bc3738b2a54aff60faf8f1f5efe3b2effabe195f
fr3632ho <fredrik@dannert.se>
2020-07-06 12:29:23 +0200

    finished 10 kinds of people in java and python

--- src/medium/10_kinds_of_people.py
+++ src/medium/10_kinds_of_people.py
@@ -1,47 +1,27 @@
 import sys
-from collections import deque
 
-def get_neighbours(graph, u, r, c, allowed):
-    n = []
-    if u % c - 1 > 0:
-        if graph[u-1] == allowed:
-            n.append(u-1)
-    if u % c + 1 < c:
-        if graph[u+1] == allowed:
-            n.append(u+1)
-    if u - c > 0:
-        if graph[u-c] == allowed:
-            n.append(u-c)
-    if u + c < r*c:
-        if graph[u+c] == allowed:
-            n.append(u + c)
-    print(n)
-    return n
+def find(p, a):
+    root = a
+    while root != p[root]: root = p[root]
+    while a != root:
+        _a = p[a]
+        p[a] = root
+        a = _a
+    return root
 
+def union(s, p, a, b):
+    a, b = find(p, a), find(p, b)
+    if a == b: return
+    if s[b] > s[a]:
+        temp = a
+        a = b
+        b = temp
 
-def BFS(graph, start, end, allowed, r, c):
-    if graph[start[0]][start[1]] != allowed or graph[end[0]][end[1]] != allowed:
-        return False
+    s[a] += s[b]
+    p[b] = a
 
-    queue = deque([start])
-    visited = set()
-
-    while queue:
-        print("in")
-        u = queue.pop()
-
-        # If path is found
-        if u == end:
-            return True
-
-        visited.add(u)
-        for i in get_neighbours(graph, u, r, c, allowed):
-            if i not in visited:
-                queue.appendleft(i)
-
-    return False
-
-
+def int_sub(a):
+    return int(a) - 1
 
 '''
 Binary has to stay inside path with zeros.
@@ -49,21 +29,27 @@ Binary has to stay inside path with zeros.
 def main():
     r, c = map(int, sys.stdin.readline().strip().split())
     graph = [sys.stdin.readline().strip() for _ in range(r)]
-    print(graph)
 
-    n = sys.stdin.readline().strip()
-    for query in range(int(n)):
-        r1, c1, r2, c2 = map(int, sys.stdin.readline().strip().split())
-        start, end = (r1-1, c1-1), (r2-1, c2-1)
-        print(start,'->' ,end)
-        print(graph[start[0]][start[1]], graph[end[0]][end[1]])
+    parents = [i for i in range(r*c)]
+    sizes = [1]*(r*c)
 
-        if BFS(graph, start, end, 0, r, c):
-            sys.stdout.write('binary\n')
-        elif BFS(graph, start, end, 1, r, c):
-            sys.stdout.write('decimal\n')
+    for i in range(r):
+        for j in range(c):
+            if j + 1 < c and int(graph[i][j]) == int(graph[i][j+1]):
+                union(sizes, parents, i*c + j, i*c + j + 1)
+            if i + 1 < r and int(graph[i][j]) == int(graph[i+1][j]):
+                union(sizes, parents, i*c + j, (i+1)*c + j)
+
+    n = int(sys.stdin.readline().strip())
+    for query in range(n):
+        r1, c1, r2, c2 = map(int_sub, sys.stdin.readline().strip().split())
+        start, end = r1*c + c1, r2*c + c2
+        s_root, e_root = find(parents, start), find(parents, end)
+        if find(parents, start) == find(parents, end):
+            print("decimal") if int(graph[r1][c1]) else print("binary")
         else:
-            sys.stdout.write('neither\n')
+            print("neither")
+
 
 if __name__ == "__main__":
     main()
