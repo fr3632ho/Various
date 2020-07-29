ff6e256e095dc210c7bdd3e32f621605f0b158fe
fr3632ho <fredrik@dannert.se>
2020-07-29 15:45:24 +0200

    finished buying cocke problem

--- src/medium/buying-coke/buying_coke.py
+++ src/medium/buying-coke/buying_coke.py
@@ -1,48 +1,38 @@
 import sys
 
-COKE = 8
-coins = [1, 5, 10]
+'''
+Dynamic programming approach to the solution of the problem.
+Using a 3-dimensional list to store previous calculations.
+'''
+def solve(min_coins, C, n1, n5, n10):
+    if min_coins[C][n5][n10] > 0:
+        return min_coins[C][n5][n10]
+    if C == 0:
+        return 0
 
-def buy_cokes(amount, cokes):
-    index = 2
-    for i in range(3):
-        if not amount[i]:
-            break
-        index = i
-
-    count = 0
-    current_value = coins[index]
-    coins_used = 1
-    while count != cokes:
-
-        i = amount[index]
-        for _ in range(i+1):
-            if count == cokes:
-                break
-
-            if current_value >= COKE:
-                amount[0] += current_value - COKE
-                count += 1
-                current_value = 0
-                continue
-
-            if amount[index]:
-                current_value += coins[index]
-                coins_used += 1
-            else:
-                break
-        index -= 1
-
-    print(coins_used)
-
+    ans = sys.maxsize
+    if n10 > 0:
+        ans = min(ans, 1 + solve(min_coins, C-1, n1+2, n5, n10-1))
+    if n5 > 1:
+        ans = min(ans, 2 + solve(min_coins, C-1, n1+2, n5-2, n10))
+    if n5 and n1 > 2:
+        ans = min(ans, 4 + solve(min_coins, C-1, n1-3, n5-1, n10))
+    if n1 > 7:
+        ans = min(ans, 8 + solve(min_coins, C-1, n1-8, n5, n10))
+    if n10 and n1 > 2: # Inserting three ones and then a ten for a return of a fiver
+        ans = min(ans, 4 + solve(min_coins, C-1, n1-3, n5+1, n10-1))
+    min_coins[C][n5][n10] = ans
+    return ans
 
 
 def main():
     T = int(input())
+    min_coins = [[[-1 for _ in range(51)]
+                               for _ in range(151)]
+                               for _ in range(151)]
     for i in range(T):
         C, n1, n5, n10 = map(int , input().split())
-        currencies = [n1, n5, n10]
-        buy_cokes(currencies, C)
+        print(solve(min_coins, C, n1, n5, n10))
 
 
 if __name__ == "__main__":
